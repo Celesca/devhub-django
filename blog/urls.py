6de2ef68f213207abf691a8b5c6090b195927791
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-02-24 11:13:22 +0700

    ADD: post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import home, post_detail
 
 urlpatterns = [
     #path('URL-name', 'function_name')
     # url-name = blank ให้เป็น homepage
-    path("", home)
+    path("", home),
+    path("posts/<int:post_id>", post_detail)
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,8 @@ def home(request):
     return render(request, 'blog/home.html', {'all_posts': all_posts})
 
 def post_detail(request, post_id):
+
+    single_post = Post.objects.get(pk=post_id)
     
     # Get single post
-    return render(request, )
+    return render(request, 'blog/post_detail.html' , {'single_post':single_post})
