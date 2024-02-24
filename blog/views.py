c68894e26527876b8f3549e3207e2d9e2c72094e
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-02-24 12:02:45 +0700

    ADD: Fetch Image from Cat_API

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 
+import requests
+
 # Create your views here.
 def home(request):
     # Get all posts Model.objects.all()
@@ -17,5 +19,28 @@ def post_detail(request, post_id):
 
 def cat_api(request):
 
+    # Endpoints ที่จะไป Request
+    url = "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_JTURr2hSvnHD4TGeVnUUuTEQgHi7BuuurpEXTXMgxLfXM27cffSItQwUv7QTUBP2"
+    API_KEY = "live_JTURr2hSvnHD4TGeVnUUuTEQgHi7BuuurpEXTXMgxLfXM27cffSItQwUv7QTUBP2"
+
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": API_KEY
+    }
+
+    res = requests.get(url, headers)
+    cats = res.json()
+    
+    cat_info = []
+    for cat in cats:
+        cat_breeds = cat.get('breeds', [])
+        if cat_breeds:
+            breed = cat_breeds[0]
+            cat_info.append({
+                'name': breed.get('name'),
+                'image': cat.get('url'),
+                'breed': breed.get('description')
+            })
+
     # pip install requests
-    return render(request, 'blog/cat.html')
+    return render(request, 'blog/cat.html', {'cats': cat_info})
